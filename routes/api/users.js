680bb1f388d59bd4e6199e5e33c9c0ab72487b45
annakoanna <anna.231991k@gmail.com>
2022-06-29 06:45:34 -0700

    finished

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,16 @@ const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 
+
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+// Insert ensureLoggedIn on all routes that need protecting
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
+
+
+
 // POST /api/users
 router.post('/', usersCtrl.create);
 router.post('/login', usersCtrl.login);
-router.get('/check-token', usersCtrl.checkToken);
 module.exports = router;
